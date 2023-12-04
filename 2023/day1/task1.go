320c581368d97d41bb3345b8f93cacb2d3c67b0f
Shane Feather-Lopez <shanedf@outlook.com>
2023-12-04 09:57:26 +0000

    day 1 / task 1 completed

--- 2023/day1/task1.go
+++ 2023/day1/task1.go
@@ -1,26 +1,53 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    file, err := os.Open("input.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-	nums := []string{"1","2","3","4","5","6","7","8","9","0"}
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-
-        fmt.Println(scanner.Text())
-    }
-
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	nums := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+	var firstIdx int
+	var firstVal string
+	var lastIdx int
+	var lastVal string
+	var sum int64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputLn := scanner.Text()
+		firstIdx = len(inputLn)
+		lastIdx = -1
+		for idx, val := range nums {
+			if (strings.Index(inputLn, val) < firstIdx) && (strings.Index(inputLn, val) > -1) {
+				fmt.Println("Found", val, "at position", idx)
+				firstIdx = strings.Index(inputLn, val)
+				firstVal = val
+			}
+			if strings.LastIndex(inputLn, val) > lastIdx {
+				fmt.Println("Found", val, "at position", idx)
+				lastIdx = strings.LastIndex(inputLn, val)
+				lastVal = val
+			}
+		}
+		tmp := firstVal + lastVal
+		fmt.Println(scanner.Text(), len(inputLn), firstVal, lastVal, tmp)
+		num, err := strconv.ParseInt(tmp, 10, 8)
+		if err != nil {
+			panic(err)
+		}
+		sum = sum + num
+	}
+	fmt.Println("Total is:", sum)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
