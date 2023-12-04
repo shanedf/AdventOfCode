da2472a8f82c480c7b0bd862fbd6d635067239ae
Shane Feather-Lopez <shanedf@outlook.com>
2023-12-04 13:57:22 +0000

    day 2 / task 1 complete

--- 2023/day2/task1.go
+++ 2023/day2/task1.go
@@ -15,35 +15,62 @@ func main() {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	var gameSum int64
+	gameSum := 0
 	limitRed := 12
 	limitGreen := 13
 	limitBlue := 14
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		input := scanner.Text()
-		var r int64
-		sumRed := 0
-		sumGreen := 0
-		sumBlue := 0
 		game := strings.Split(input, ":")[0]
 		gameId := strings.Fields(game)[1]
-		rounds := strings.Split(game, ";")
+		rounds := strings.Split(strings.Split(input, ":")[1], ";")
+		trip := false
 		for _, round := range rounds {
 			draw := strings.Split(round, ",")
 			for _, clr := range draw {
+				var r int64
 				res := strings.Fields(strings.TrimSpace(clr))
 				switch res[1] {
 				case "red":
 					r, err = strconv.ParseInt(res[0], 10, 8)
-				}
-				if err != nil {
-					panic(err)
+					if err != nil {
+						panic(err)
+					}
+					if int(r) > limitRed {
+						trip = true
+					}
+				case "green":
+					r, err = strconv.ParseInt(res[0], 10, 8)
+					if err != nil {
+						panic(err)
+					}
+					if int(r) > limitGreen {
+						trip = true
+					}
+				case "blue":
+					r, err = strconv.ParseInt(res[0], 10, 8)
+					if err != nil {
+						panic(err)
+					}
+					if int(r) > limitBlue {
+						trip = true
+					}
 				}
 			}
+
+		}
+		if trip == false {
+			fmt.Println("Game", gameId, "is possible")
+			var id int64
+			id, err = strconv.ParseInt(gameId, 10, 8)
+			if err != nil {
+				panic(err)
+			}
+			gameSum = gameSum + int(id)
 		}
 	}
-	fmt.Println("Total is:", sum)
+	fmt.Println("Total is:", gameSum)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
