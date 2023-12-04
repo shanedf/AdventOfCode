997096441470481b3556c8bc8ec495b09ee1d0c0
Shane Feather-Lopez <shanedf@outlook.com>
2023-12-04 15:00:19 +0000

    day 2 / task 2 complete

---
+++ 2023/day2/task2.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	gameSum := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text()
+		rounds := strings.Split(strings.Split(input, ":")[1], ";")
+		minRed := 0
+		minGreen := 0
+		minBlue := 0
+		for _, round := range rounds {
+			draw := strings.Split(round, ",")
+			for _, clr := range draw {
+				var r int64
+				res := strings.Fields(strings.TrimSpace(clr))
+				switch res[1] {
+				case "red":
+					r, err = strconv.ParseInt(res[0], 10, 8)
+					if err != nil {
+						panic(err)
+					}
+					if int(r) > minRed {
+						minRed = int(r)
+					}
+				case "green":
+					r, err = strconv.ParseInt(res[0], 10, 8)
+					if err != nil {
+						panic(err)
+					}
+					if int(r) > minGreen {
+						minGreen = int(r)
+					}
+				case "blue":
+					r, err = strconv.ParseInt(res[0], 10, 8)
+					if err != nil {
+						panic(err)
+					}
+					if int(r) > minBlue {
+						minBlue = int(r)
+					}
+				}
+			}
+		}
+		gameSum = gameSum + (minRed * minGreen * minBlue)
+	}
+	fmt.Println("Total is:", gameSum)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
