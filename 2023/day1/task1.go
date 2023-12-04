17096e014845c65d2cea12cc4200d8d5c7d5317c
Shane Feather <shanedf@outlook.com>
2023-12-04 07:46:16 +0000

    add search array for numbers

--- 2023/day1/task1.go
+++ 2023/day1/task1.go
@@ -13,10 +13,10 @@ func main() {
         log.Fatal(err)
     }
     defer file.Close()
-
+	nums := []string{"1","2","3","4","5","6","7","8","9","0"}
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
-		
+
         fmt.Println(scanner.Text())
     }
 
