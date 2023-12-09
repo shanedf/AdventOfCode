ec03637d03e73b347e0061e6515f63f7aa0ac80f
Shane Feather-Lopez <shanedf@outlook.com>
2023-12-09 06:34:08 +0000

    processed input array

--- 2023/day4/task1.go
+++ 2023/day4/task1.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"slices"
 	"strings"
 )
 
@@ -17,15 +18,37 @@ func main() {
 	re := regexp.MustCompile("[0-9]+")
 	scanner := bufio.NewScanner(file)
 	card := 0
-	var cards = [198][]int
-	var draws = [198][]int
-	var matches = [198]int
+	var cards [198][]string
+	var draws [198][]string
+	var matches [198][]string
 	total := 0
 	for scanner.Scan() {
 		inputLn := strings.Split(strings.Split(scanner.Text(), ":")[1], "|")
-		strCard := re.FindAllString(inputLn[1])
-		strDraw := re.FindAllString(inputLn[0])
-
+		cards[card] = re.FindAllString(inputLn[1], -1)
+		draws[card] = re.FindAllString(inputLn[0], -1)
+		// var intCard = []int{}
+		// for _, c := range strCard {
+		// 	v, err := strconv.Atoi(c)
+		// 	if err != nil {
+		// 		panic(err)
+		// 	}
+		// 	intCard = append(intCard, v)
+		// }
+		// cards[card] = intCard
+		// var intDraw = []int{}
+		// for _, d := range strDraw {
+		// 	v, err := strconv.Atoi(d)
+		// 	if err != nil {
+		// 		panic(err)
+		// 	}
+		// 	intDraw = append(intDraw, v)
+		// }
+		// draws[card] = intDraw
+		for _, v := range draws[card] {
+			if slices.Contains(cards[card], v) {
+				matches[card] = append(matches[card], v)
+			}
+		}
 		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
 		}
